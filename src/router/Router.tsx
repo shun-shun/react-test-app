1ced33f6707b20562027d2bef82c1e2f16d37792
shun-shun <gondo.syun0711@gmail.com>
2023-08-01 19:52:04 +0900

    ログイン画面完成

---
+++ src/router/Router.tsx
@@ -0,0 +1,41 @@
+import { FC, memo } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/pages/Login";
+import { UserManagement } from "../components/pages/UserManagement";
+import { Setting } from "../components/pages/Setting";
+import { Home } from "../components/pages/Home";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+
+export const Router: FC = memo(() => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/home"
+        element={
+          <HeaderLayout>
+            <Home />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="/home/user_management"
+        element={
+          <HeaderLayout>
+            <UserManagement />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="/home/setting"
+        element={
+          <HeaderLayout>
+            <Setting />
+          </HeaderLayout>
+        }
+      />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+});
