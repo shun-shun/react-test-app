a2862b22da4473e36737f2536211b01b50110202
shun-shun <gondo.syun0711@gmail.com>
2023-08-01 23:36:00 +0900

    ユーザ一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,41 @@
-import { FC, memo } from "react";
+import {
+  Box,
+  Center,
+  Image,
+  Spinner,
+  Stack,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザ管理画面ページ</p>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
